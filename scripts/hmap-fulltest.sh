a973ef4327e720b8630995afa4c57504201456c1
Cristobal <cristobal.navarro.g@gmail.com>
2023-03-04 00:32:42 -0300

    improved script

--- scripts/hmap-fulltest.sh
+++ scripts/hmap-fulltest.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Run as"
-    printf "     ${0} <dev> <testname>\n\n"
-    printf "e.g: ${0}  0     RTX3090Ti\n\n"
+    printf "     ${0} <dev> <nt> <testname>\n\n"
+    printf "e.g: ${0}  0      8   RTX3090Ti\n\n"
     exit
 fi
 dev=${1}
-testname=${2}
-printf "dev=${0}  testname=${testname}"
+nt=${2}
+testname=${3}
+printf "dev=${0}  nt=${nt}  testname=${testname}"
 # small n, many rea/reps
-./hmap-benchmark.sh ${dev} 8 5  16  16    10 14   26   0 24  0 24  ${testname}
+./hmap-benchmark.sh ${dev} ${nt} 5  16  16     0 12   26   0 24  0 24  ${testname}
 
 # medium n, intermediate number of rea/reps
-./hmap-benchmark.sh ${dev} 8 5  8    8    15 19   26   0 24  0 24  ${testname}
+./hmap-benchmark.sh ${dev} ${nt} 5  8    8    13 19   26   0 24  0 24  ${testname}
 
 # large n, small number of rea/reps
-./hmap-benchmark.sh ${dev} 8 5  4    4    20 24   26   0 24  0 24  ${testname}
+./hmap-benchmark.sh ${dev} ${nt} 5  4    4    20 24   26   0 24  0 24  ${testname}
