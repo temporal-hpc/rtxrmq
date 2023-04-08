10a09cd0729ee840341b8befed8e96023897f949
cristobal <cristobal.navarro.g@gmail.com>
2023-04-08 12:56:34 -0400

    new perf vs q script in exponential steps

---
+++ scripts/qperf-fulltest-exp.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+if [ "$#" -ne 4 ]; then
+    echo "Run as"
+    printf "     ${0} <dev> <nt> <alg> <filename>\n"
+    printf "e.g: ${0}   0     8    5    RTX4090\n"
+    printf "\nnote:\n"
+    printf "  - dev   : GPU device ID\n"
+    printf "  - nt    : number of CPU threads (relevant for CPU methods)\n"
+    printf "  - alg   : algorithm (check ./rtxrmq help message)\n"
+    exit
+fi
+dev=${1}
+nt=${2}
+alg=${3}
+name=${4}
+const_bs=15
+const_nb=9
+DATEBEGIN=$(exec date +"%T-%m-%d-%Y (%:z %Z)")
+echo "START #DATE = ${DATEBEGIN}"
+printf "args dev=${dev} nt=${nt} alg=${alg} bsize=${bsize} name=${name}\n\n"
+if [ "$alg" -ne 5 ]; then
+	for lr in {-1..-3}
+	do
+			#./perf-benchmark-exp.sh <dev> <nt>  <alg>  <rea> <reps>   <n1> <n2>  <q1> <q2>  <bs-or-nb> <bsize>   <lr> <filename>
+			./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       nb       0      ${lr}  ${name}
+	done
+else
+	# UNIFORM DISTRIBUTION (large values)
+	#./perf-benchmark-exp.sh <dev> <nt>  <alg>  <rea> <reps>   <n1> <n2>  <q1> <q2>  <bs-or-nb> <bsize>   <lr> <filename>
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       nb         9      -1   ${name}
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       bs   ${const_bs}  -1   ${name}-constBS
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       nb   ${const_nb}  -1   ${name}-constNB
+
+	# LOGNORMAL DISTRIBUTION (medium values) EXP 0.6
+	#./perf-benchmark-exp.sh <dev> <nt>  <alg>  <rea> <reps>   <n1> <n2>  <q1> <q2>  <bs-or-nb> <bsize>   <lr> <filename>
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       nb         1      -2   ${name}
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       bs   ${const_bs}  -2   ${name}-constBS
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       nb   ${const_nb}  -2   ${name}-constNB
+
+	# LOGNORMAL DISTRIBUTION (small values) EXP 0.3
+	#./perf-benchmark-exp.sh <dev> <nt>  <alg>  <rea> <reps>   <n1> <n2>  <q1> <q2>  <bs-or-nb> <bsize>   <lr> <filename>
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       nb        13      -3   ${name}
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       bs   ${const_bs}  -3   ${name}-constBS
+	./perf-benchmark-exp.sh ${dev} ${nt} ${alg}  16     32      24   24     0   26       nb   ${const_nb}  -3   ${name}-constNB
+fi
+DATEEND=$(exec date +"%T-%m-%d-%Y (%:z %Z)")
+printf "FULL BENCHMARK EXP FINISHED: args dev=${dev} nt=${nt} alg=${alg} name=${name}\n"
+printf "\tBEGIN: ${DATEBEGIN}\n\tEND: ${DATEEND}\n\n"
