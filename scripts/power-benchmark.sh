65f9f5c1253ce6b094c1e7de07f46917ff87899e
Cristobal <cristobal.navarro.g@gmail.com>
2023-04-20 12:18:32 -0400

    power benchmark first version

--- scripts/power-benchmark.sh
+++ scripts/power-benchmark.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-if [ "$#" -ne 12 ]; then
+if [ "$#" -ne 10 ]; then
     echo "Run as"
-    printf "     ${0} <dev> <nt> <alg> <rea> <reps>   <n> <q> <bsize> <lr> <filename>\n\n"
-    printf "e.g: ${0}  0     8     5     8     10      26  26   15     20   RTX3090Ti\n"
+    printf "     ${0} <dev> <nt> <alg> <seed> <reps>    <n>          <q>         <nb>    <lr> <filename>\n\n"
+    printf "e.g: ${0}  0     8     5    1321    16  \$((2**20))  \$((2**26))  \$((2**9))   -2   RTX4090\n"
     printf "\nnote:\n"
     printf "  - the *.csv extension will be placed automatically\n"
+    printf "  - 'nb' (number of blocks) will only be considered for algorithms 5 and 8\n"
     printf "  - prefix (power) and suffix (alg) will be added to filename\n"
-    printf "  - n,q,bsize,lr values are exponents of 2^x\n"
-    printf "  - lr is the actual query range size\n\n"
+    printf "  - 'lr' > 0 is the actual query range size, i.e., lr = (r-l)+1\n"
+    printf "  - 'lr' < 0 the distributions (-1 large,-2 medium,-3 small)\n\n"
+    printf "  - NOTE: check first that the result is correct (--check param)\n\n"
     exit
 fi
 dev=${1}
 nt=${2}
 alg=${3}
-rea=${4}
+seed=${4}
 reps=${5}
 n=${6}
 q=${7}
-bsize=${8}
+nb=${8}
 lr=${9}
 outfile_path=../data/power-${10}-n${n}-q${q}-lr${lr}-ALG${alg}.csv
 binary=./rtxrmq
 
-printf "args:\ndev=${dev} nt=${nt} alg=${alg} rea=${rea} reps=${reps} n=${n} q=${q} bsize=${bsize} lr=${lr} outfile_path=${outfile_path}\n\n"
-#[ ! -f ${outfile_path} ] && echo "dev,alg,reps,n,bs,q,lr,t,q/s,ns/q,construction" > ${outfile_path}
+printf "args:\ndev=${dev} nt=${nt} alg=${alg} rea=${rea} reps=${reps} n=${n} q=${q} nb=${nb} lr=${lr} outfile_path=${outfile_path}\n\n"
 DATE=$(exec date +"%T-%m-%d-%Y (%:z %Z)")
 echo "START #DATE = ${DATE}"
 
 # change to bin directory
 cd ../build
 
-SEED=${RANDOM}
-printf "${binary} $((2**$n)) $((2**$q)) ${lr} ${alg} --bs ${bsize} --reps $reps --nt $nt --dev $dev --save-power=${outfile_path} --seed ${SEED}\n"
-        ${binary} $((2**$n)) $((2**$q)) ${lr} ${alg} --bs ${bsize} --reps $reps --nt $nt --dev $dev --save-power=${outfile_path} --seed ${SEED}\n
+printf "${binary} $n $q ${lr} ${alg} --nb ${nb} --reps $reps --nt $nt --dev $dev --save-power=${outfile_path} --seed ${seed}\n"
+        ${binary} $n $q ${lr} ${alg} --nb ${nb} --reps $reps --nt $nt --dev $dev --save-power=${outfile_path} --seed ${seed}
 # come back to scripts directory
 cd ../scripts
 DATE=$(exec date +"%T-%m-%d-%Y (%:z %Z)")
